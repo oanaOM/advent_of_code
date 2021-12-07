69acfa84cc0cd109445dfcf4a8cd5d2b1f1413eb
oanaom <hello2oana@yahoo.com>
2021-12-07 23:14:13 +0000

    make day 7 shorter

--- 2021/js/day07/day07.js
+++ 2021/js/day07/day07.js
@@ -1,43 +1,15 @@
 var fs = require("fs");
 
 fs.readFile("./input.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  if (err) console.error(err);
 
-  console.log(partOne(data));
-  console.log(partTwo(data));
-});
-
-function partOne(data) {
   data = data.trim().split(",").map(Number);
+  let positionsA, positionsB = [];
 
-  let minPos = Math.min(...data);
-  let maxPos = Math.max(...data);
-  let costs = [];
-
-  for (let i = minPos; i < maxPos; i++) {
-    costs.push(data.map((x) => Math.abs(x - i)).reduce((p, v) => p + v));
-  }
-
-  return Math.min(...costs);
-}
-function partTwo(data) {
-  data = data.trim().split(",").map(Number);
-
-  let minPos = Math.min(...data);
-  let maxPos = Math.max(...data);
-  let costs = [];
-
-  for (let i = minPos; i < maxPos; i++) {
-    costs.push(
-      data
-        .map((x) => Math.abs(x - 5))
-        .map((x) => (x * (x + 1)) / 2)
-        .reduce((p, v) => p + v)
+  for (let i = Math.min(...data); i < Math.max(...data); i++) {
+    positionsA.push(data.map((x) => Math.abs(x - i)).reduce((p, v) => p + v));
+    positionsB.push(data.map((x) => Math.abs(x - i)).map((x) => (x * (x + 1)) / 2).reduce((p, v) => p + v)
     );
   }
-
-  return Math.min(...costs);
-}
+  return console.log(`${Math.min(...positionsA)},  ${Math.min(...positionsB)}`);
+});
