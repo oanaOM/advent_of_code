f00c9436cd469b5d89043950540b43a5df1c1b1f
oanaom <hello2oana@yahoo.com>
2021-12-07 23:16:06 +0000

    return proper value

--- 2021/js/day07/day07.js
+++ 2021/js/day07/day07.js
@@ -4,12 +4,13 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
   if (err) console.error(err);
 
   data = data.trim().split(",").map(Number);
-  let positionsA, positionsB = [];
+  let positionsA = [];
+  let positionsB = [];
 
   for (let i = Math.min(...data); i < Math.max(...data); i++) {
     positionsA.push(data.map((x) => Math.abs(x - i)).reduce((p, v) => p + v));
     positionsB.push(data.map((x) => Math.abs(x - i)).map((x) => (x * (x + 1)) / 2).reduce((p, v) => p + v)
     );
   }
-  return console.log(`${Math.min(...positionsA)},  ${Math.min(...positionsB)}`);
+  return `${Math.min(...positionsA)},  ${Math.min(...positionsB)}`;
 });
